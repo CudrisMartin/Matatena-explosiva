0be4d8ee4c53bb33a6aae9ef87f53db085b0b395
CudrisMartin <09azza.meca@gmail.com>
2022-11-22 16:43:17 -0500

    Inicialización main.py

--- Matatena_explosiva_proyecto/main.py
+++ Matatena_explosiva_proyecto/main.py
@@ -1,1 +1,28 @@
 import pygame
+
+
+###VARIABLES GLOBALES###
+##RECURSOS##
+fondo =  pygame.image.load('Sprites\Fondo.jpg')
+pygame.display.set_caption("Matatena explosiva")
+#Colores#
+#Resolución#
+w = 512
+h = 300
+#Variables importantes#
+FPS = 30
+#Variables auxiliares#
+Clock = pygame.time.Clock()
+#Estados#
+gameover = False
+play = False
+dificultad = 1
+
+def recargar_pantalla():
+    global play, gameover, dificultad
+    Pantalla.blit(fondo,(fondo.get_rect().width,0))
+
+pygame.init()
+
+Pantalla = pygame.display.set_mode((w,h))
+Clock = pygame.time.Clock

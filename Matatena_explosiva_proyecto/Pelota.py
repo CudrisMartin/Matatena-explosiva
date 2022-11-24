9447d44822dcf6e54bf40bff00e8899c254bc811
Megameca04 <09azza.meca@gmail.com>
2022-11-24 16:00:48 -0500

    Avances en pelota

---
+++ Matatena_explosiva_proyecto/Pelota.py
@@ -0,0 +1,24 @@
+import pygame
+
+sprites = [
+    pygame.image.load('Sprites\Pelota\Pelota.png'),
+    pygame.image.load('Sprites\Pelota\Pelota explotada.png')
+]
+
+pelota = sprites[0].convert()
+
+if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.button == pygame.BUTTON_LEFT:
+         if true:
+          if pelota.bottom > suelo:
+            pelota.move_ip(0,5)
+          elif pelota.bottom < suelo:
+            pelota.bottom = suelo
+      
+      if pelota == suelo:
+        pelota.bottom < center:
+        pelota.move_ip(0,-4)
+      elif pelota.bottom > center:
+        pelota.bottom = center
+        If perdiste == true:
+           Break
